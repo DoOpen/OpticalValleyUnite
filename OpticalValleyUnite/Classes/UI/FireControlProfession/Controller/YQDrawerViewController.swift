881fbf93fa6309e4ee2ac4979764e09040ad3024
杨庆庆 <972544908@qq.com>
2017-09-15 16:09:42 +0800

    消防抽屉界面init_OK

--- OpticalValleyUnite/Classes/UI/FireControlProfession/Controller/YQDrawerViewController.swift
+++ OpticalValleyUnite/Classes/UI/FireControlProfession/Controller/YQDrawerViewController.swift
@@ -9,12 +9,69 @@
 import UIKit
 
 class YQDrawerViewController: UIViewController {
+    
+    // MARK: - 各类属性的列表
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBOutlet weak var joinNum: UIButton!
 
+    @IBOutlet weak var fireNum: UIButton!
+    
+    
+    @IBOutlet weak var falsePositerNum: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //1.调用获取用户信息的接口
+        
+        
+        //2.调用火警数量的接口
        
     }
+    
+    // MARK: - 各个按钮点击实现的方法
+    /// 参与总单数详情
+    @IBAction func joinTotallNum(_ sender: Any) {
+        
+        
+    }
+    
+    /// 火警单详情
+    @IBAction func fireAlarm(_ sender: UITapGestureRecognizer) {
+        
+        
+    }
+   
+    /// 误报单详情
+    @IBAction func falsePositives(_ sender: UITapGestureRecognizer) {
+        
+        
+        
+    }
+    
+    /// 系统选择
+    @IBAction func systemSelect(_ sender: UITapGestureRecognizer) {
+        
+        
+        
+    }
+    
+    /// 修改密码
+    @IBAction func pwdModify(_ sender: UITapGestureRecognizer) {
+        
+        
+        
+    }
+    
+    /// 退出登录
+    @IBAction func eixtLoyout(_ sender: UITapGestureRecognizer) {
+        
+        
+        
+    }
+    
+    
 
    
 }

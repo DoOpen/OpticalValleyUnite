1be80aecf59e00779a28c10b7ad517af443c1699
杨庆庆 <972544908@qq.com>
2017-10-12 15:55:05 +0800

    友盟推送的逻辑,思路代码重构,整理

--- OpticalValleyUnite/AppDelegate.swift
+++ OpticalValleyUnite/AppDelegate.swift
@@ -29,27 +29,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         IQKeyboardManager.shared().isEnabled = true
         SVProgressHUD.setMaximumDismissTimeInterval(2)
         
+        /*
+         1. 集成友盟的sdk,步骤方法
+         2. 通过的是 适配Https
+         */
         UMessage.start(withAppkey: Const.SJUMPushKey, launchOptions: launchOptions)
+        //注册通知，如果要使用category的自定义策略，可以参考demo中的代码。
         UMessage.registerForRemoteNotifications()
         UMessage.setBadgeClear(false)
 
-        
+        // 3.如果是ios10 必须加上 如下的代码
         if #available(iOS 10.0, *) {
             
             let center = UNUserNotificationCenter.current()
+            //设置 友盟的代理的情况
             center.delegate = self
             //let types =
             center.requestAuthorization(options: [.alert, .sound, .badge], completionHandler: { granted,error in
+                //文档中的继承 granted  通过的逻辑的执行来进行的判断 允许点击和 不允许的点击的逻辑判断
                 
             })
             
         } else {
             
+            /// iOS 10.0 以下的代码,它重新的设置了 系统通知的类型方法
             let settings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            
             application.registerUserNotificationSettings(settings)
             application.registerForRemoteNotifications()
         }
         
+        
+        //打开日志，方便调试
         UMessage.setLogEnabled(true)
         
         return true
@@ -84,8 +95,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 /// 集成的友盟的接口
 extension AppDelegate: UNUserNotificationCenterDelegate{
+    
     //iOS10以下使用这个方法接收通知
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        
         UMessage.didReceiveRemoteNotification(userInfo)
         
         noticHandel(userInfo: userInfo)
@@ -113,6 +126,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate{
         
     }
     
+//iOS10新增：处理前台收到通知的代理方法
     ////iOS10新增：处理前台收到通知的代理方法
     @available(iOS 10.0, *)
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
@@ -133,10 +147,13 @@ extension AppDelegate: UNUserNotificationCenterDelegate{
     
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
         let token = NSData(data: deviceToken).description.replacingOccurrences(of: "<", with: "").replacingOccurrences(of: ">", with: "").replacingOccurrences(of: " ", with: "")
+        
         print(token)
         
         UserDefaults.standard.set(token, forKey: "SJDeviceToken")
+        
         if let _ = UserDefaults.standard.object(forKey: "HasToken"){
             
         }
@@ -149,8 +166,12 @@ extension AppDelegate: UNUserNotificationCenterDelegate{
         }
 }
 
+
+
+/// 继承appDelegate的点击进行的界面间跳转的方法
 extension AppDelegate{
     
+    // MARK: - 推送通知返回的 值--->(关联后台的参数值来解析) 进行相应的跳转传值
     func noticHandel(userInfo: [AnyHashable : Any]){
         
         if !User.isLogin(){
@@ -164,27 +185,34 @@ extension AppDelegate{
                 if let sub_type = userInfo["sub_type"] as? String,sub_type == "督办"{
                     let workId = userInfo["workId"] as! String
                     pushToDubanViewController(workId)
+                    
                 }else{
                     let workId = userInfo["id"] as! String
                     pushToWorkOrderViewController(workId)
                 }
                 
             }else if type == "通知"{
+                
                 pushToHomeViewController()
             }
         }
     }
     
+    
     func getNavController(_ vc: UIViewController = (SJKeyWindow!.rootViewController)!) -> UINavigationController?{
+        
         if let vc = vc as? UITabBarController{
+            
             return getNavController(vc.selectedViewController!)
         }else if let vc = vc as? UINavigationController{
+            
             return vc
         }
         return nil
 
     }
     
+    // MARK: - push到 工单的界面
     func pushToWorkOrderViewController(_ workId: String){
         
         let vc = WorkOrderProgressViewController.loadFromStoryboard(name: "WorkOrder") as! WorkOrderProgressViewController
@@ -194,6 +222,7 @@ extension AppDelegate{
         getNavController()?.pushViewController(vc, animated: true)
     }
     
+    // MARK: - push到 督办的界面
     func pushToDubanViewController(_ workId: String){
 
         let vc = WorkOrderProgressViewController.loadFromStoryboard(name: "WorkOrder") as! WorkOrderProgressViewController
@@ -205,6 +234,7 @@ extension AppDelegate{
         getNavController()?.pushViewController(vc, animated: true)
     }
     
+    // MARK: - push到 主界面的情况
     func pushToHomeViewController(){
         if let vc = SJKeyWindow!.rootViewController as? UITabBarController{
             if vc.selectedIndex == 0{

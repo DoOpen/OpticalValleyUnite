2272e4d145275e82ac66ce1f1d26b66e7485b095
杨庆庆 <972544908@qq.com>
2018-04-05 11:58:59 +0800

    分享内容视图的方法swift翻译,内容层次重构,优化;

--- OpticalValleyUnite/Classes/UI/PersonCore/ViewController/YQShareViewController.swift
+++ OpticalValleyUnite/Classes/UI/PersonCore/ViewController/YQShareViewController.swift
@@ -39,20 +39,46 @@ class YQShareViewController: UIViewController {
     
     //MARK: - leftBarItemButtonClick方法
     func addRightBarItemButtonClick(){
-        
-        //调用setPreDefinePlatforms的示例
-//        UMSocialUIManager.setPreDefinePlatforms(
-//            [NSNumber(integerLiteral:UMSocialPlatformType.wechatSession.rawValue)
-//                ,NSNumber(integerLiteral:UMSocialPlatformType.QQ.rawValue)
-//                ,NSNumber(integerLiteral:UMSocialPlatformType.sina.rawValue)
-//            ]
-//        )
 
-        UMShareSwiftInterface.showShareMenuViewInWindowWithPlatformSelectionBlock { (_, nil) in
+        UMShareSwiftInterface.showShareMenuViewInWindowWithPlatformSelectionBlock { (platformType : UMSocialPlatformType, nil) in
             
+            self.shareWebPageToPlatformType(platformType: platformType)
         }
     }
 
+    //MARK: - 友盟分享内容视图方法
+    func shareWebPageToPlatformType(platformType : UMSocialPlatformType){
     
+        //封装UM的消息体的情况
+        let messageObject = UMSocialMessageObject()
+        let thumbURL = "https://mobile.umeng.com/images/pic/home/social/img-1.png"
+        let shareObject = UMShareWebpageObject.shareObject(withTitle: "欢迎使用【友盟+】社会化组件U-Share", descr: "欢迎使用【友盟+】社会化组件U-Share，SDK包最小，集成成本最低，助力您的产品开发、运营与推广！", thumImage: thumbURL)
+        shareObject?.webpageUrl = "http://mobile.umeng.com/social"
+        
+        messageObject.shareObject = shareObject
+        
+        UMShareSwiftInterface.share(plattype: platformType, messageObject: messageObject, viewController: self) { (data, error) in
+            
+            if ((error) != nil) {
+                
+//                UMSocialLog("************Share fail with error %@*********")
+                
+            }else{
+                
+                if ((data) != nil) {
+                    
+//                    let resp = data as! UMSocialShareResponse;
+                    //分享结果消息
+//                    UMSocialLogInfo("response message is %@",resp.message);
+                    //第三方原始返回的数据
+//                    UMSocialLogInfo("response originalResponse data is %@",resp.originalResponse);
+                    
+                }else{
+                    
+//                    UMSocialLogInfo("response data is %@",data);
+                }
+            }
+        }
+    }
 
 }

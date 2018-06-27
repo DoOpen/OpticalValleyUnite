e84abcc9b3bb3b00ffae7f230e48bf464c925e73
杨庆庆 <972544908@qq.com>
2018-06-27 11:30:02 +0800

    知识库查看已出成绩详情界面init;

---
+++ OpticalValleyUnite/Classes/UI/KnowledgeLibrary/View/YQCheckAchievementDetailV.swift
@@ -0,0 +1,20 @@
+//
+//  YQCheckAchievementDetailV.swift
+//  OpticalValleyUnite
+//
+//  Created by 杨庆 on 2018/6/27.
+//  Copyright © 2018年 yangqing. All rights reserved.
+//
+
+import UIKit
+
+class YQCheckAchievementDetailV: UIView {
+
+    //思路是: 添加这个view的边框边线,并且设置属性参数
+    
+    @IBOutlet weak var optionLabel: UILabel!
+    
+    @IBOutlet weak var selectButton: UIButton!
+    
+
+}
---
+++ OpticalValleyUnite/Classes/UI/KnowledgeLibrary/ViewController/YQCheckAchievementDetailVC.swift
@@ -0,0 +1,40 @@
+//
+//  YQCheckAchievementDetailVC.swift
+//  OpticalValleyUnite
+//
+//  Created by 杨庆 on 2018/6/27.
+//  Copyright © 2018年 yangqing. All rights reserved.
+//
+
+import UIKit
+
+class YQCheckAchievementDetailVC: UIViewController {
+
+    //题目view
+    
+    @IBOutlet weak var scrollContentView: UIView!
+    
+    @IBOutlet weak var scrollContentHightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var answerTitleLabel: UILabel!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    
+    }
+
+
+    // MARK: - 点击下一题
+    
+    @IBAction func nextButtonClick(_ sender: UIButton) {
+        
+        
+    }
+    
+
+}

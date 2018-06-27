eb79740b453be9dc228b4f001e0225e0d0f9bc5b
杨庆庆 <972544908@qq.com>
2018-06-27 16:35:20 +0800

    知识库简答题init;知识库多选题的界面逻辑重构调整;

---
+++ OpticalValleyUnite/Classes/UI/KnowledgeLibrary/View/YQShortAnswerQuestionsV.swift
@@ -0,0 +1,17 @@
+//
+//  YQShortAnswerQuestionsV.swift
+//  OpticalValleyUnite
+//
+//  Created by 杨庆 on 2018/6/27.
+//  Copyright © 2018年 yangqing. All rights reserved.
+//
+
+import UIKit
+
+class YQShortAnswerQuestionsV: UIView {
+
+    
+    @IBOutlet weak var shortAnswerTextView: UITextView!
+    
+    
+}
